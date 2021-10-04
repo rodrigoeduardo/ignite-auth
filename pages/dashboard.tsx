d68685eeb15b4c8066c911104ed3ab2d520d5367
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-10-04 18:37:06 -0300

    setting dashboard page as default

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { AuthContext } from "../contexts/AuthContext"
 
-export function Dashboard() {
+export default function Dashboard() {
     const { user } = useContext(AuthContext)
 
     return <h1>Dashboard: {user?.email}</h1>
