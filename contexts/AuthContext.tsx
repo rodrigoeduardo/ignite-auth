f7dfe60569cd8e51efb584fef1ddfd2a8b44c8b4
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-10-04 18:36:29 -0300

    storaging token in cookies and signout handling

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies, destroyCookie } from 'nookies';
 import Router from "next/router";
 
 import { api } from "../services/api";
@@ -26,9 +27,30 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut() {
+    destroyCookie(undefined, 'igniteauth.token')
+    destroyCookie(undefined, 'igniteauth.refreshToken')
+
+    Router.push('/')
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>();
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        const { 'igniteauth.token': token } = parseCookies()
+
+        if (token) {
+            api.get('/me').then(response => {
+                const { email, permissions, roles } = response.data
+
+                setUser({ email, permissions, roles })
+            }).catch(() => {
+                signOut()
+            })
+        }
+    }, [])
 
     async function signIn({ email, password }: SignInCredentials) {
         try {
@@ -37,13 +59,24 @@ export function AuthProvider({ children }: AuthProviderProps) {
                 password
             })
 
-            const { permissions, roles } = response.data
+            const { token, refreshToken, permissions, roles } = response.data
+
+            setCookie(undefined, 'igniteauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            })
+            setCookie(undefined, 'igniteauth.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            })
 
             setUser({
                 email,
                 permissions,
                 roles
             })
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
             Router.push('/dashboard')
         } catch (err) {
